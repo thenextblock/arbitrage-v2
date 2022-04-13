a07ee42a472bc5bc153d5ad91aa7bbe3c97cb5ac
achiko <achiko@gmail.com>
2022-04-13 17:06:27 +0400

    last fixes

--- src/pairCreatedEvents.ts
+++ src/pairCreatedEvents.ts
@@ -6,10 +6,18 @@ import { getV2Pairs } from "./graph/uniswapv2grap";
 import { storePair } from "./db";
 import * as _ from "lodash";
 import Queue from "bull";
-const DB_QUEE = new Queue("db");
+import args from "args";
+import "colors";
 
+args
+  .option("from", "Starting Block")
+  .option("size", "paging size", 500000)
+  .option("page", "page", 0)
+  .option("concurency", "how many paralelt Procces you need", 488);
+
+const DB_QUEE = new Queue("db");
 const RPC_HOST = process.env.RPC_HTTP || "";
-const RPC_WS = process.env.RPC_WS || "";
+// const RPC_WS = process.env.RPC_WS || "";
 
 const EXCHNAGE_NAME = "Uniswap_V2";
 const UNISWAP_V2_FACTORY_ADDRESS = "0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f";
@@ -17,11 +25,32 @@ const UNISWAP_V2_FACTORY_ADDRESS = "0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f";
 const provider = new ethers.providers.JsonRpcProvider(RPC_HOST);
 // const wsProvider = new ethers.providers.WebSocketProvider(RPC_WS);
 
+let QUEE_CONCURRENCY = 500;
+
 (async () => {
-  console.log("RPC : ", RPC_HOST);
+  console.log("RPC: ", RPC_HOST);
+  DB_QUEE.empty();
+  const flags = args.parse(process.argv);
+  const { from, size, page, concurency } = flags;
+  QUEE_CONCURRENCY = concurency;
+
+  if (!from) {
+    console.log(`Please provide Starting Block`.red);
+    return;
+  }
+
+  if (!page) {
+    console.log(`Please provide  Page Number -P`.red);
+    return;
+  }
+
+  console.log("-".repeat(80).gray);
+  console.log(`PROCESS -> From ${from}, Size: ${size} Page: ${page}, Concurency: ${concurency} `.green);
+  console.log("-".repeat(80).gray);
+
+  await queeMonitor(DB_QUEE);
   // console.log("Ethers: ", utils.toUtf8String("0x4156540000000000000000000000000000000000000000000000000000000000"));
-  await pairCreatedEvents();
-  // await getpairsViaGraph();
+  await pairCreatedEvents(from, size, page - 1);
 })();
 
 export async function getpairsViaGraph() {
@@ -45,16 +74,19 @@ export async function getpairsViaGraph() {
   }
 }
 
-export async function pairCreatedEvents() {
+export async function pairCreatedEvents(_startBlock: number, _size: number, _page: number) {
   console.log("PAIR CREATED EVENTS ... ");
-  const comptroller = UniswapV2FactoryAbi__factory.connect(UNISWAP_V2_FACTORY_ADDRESS, provider);
-  let eventFilter = comptroller.filters.PairCreated();
+  const uniswapFactory = UniswapV2FactoryAbi__factory.connect(UNISWAP_V2_FACTORY_ADDRESS, provider);
+  let eventFilter = uniswapFactory.filters.PairCreated();
 
-  let startBlock = 10000835;
-  let paging = 500000;
-  let page = 0;
+  //TODO : Optimize this shitty Code here
+  let startBlock = _startBlock;
+  let paging = _size;
+  let page = _page;
 
-  let events = await comptroller.queryFilter(
+  console.log(`Events From: ${startBlock + paging * page} To: ${startBlock + paging * page + paging}`.yellow);
+
+  let events = await uniswapFactory.queryFilter(
     eventFilter,
     startBlock + paging * page,
     startBlock + paging * page + paging
@@ -64,25 +96,9 @@ export async function pairCreatedEvents() {
   console.log("All Events : ", events.length);
 
   events.map((event: any) => {
-    // token0: '0x2D80f5F5328FdcB6ECeb7Cacf5DD8AEDaEC94e20',
-    // token1: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    // pair: '0xAb360Cad76140B1D2bfee8BB2797d8424de52063'
-
     let { token0, token1, pair } = event.args;
-
     DB_QUEE.add({ token0, token1, pair });
-    console.log("Pair : ", pair);
-    console.log("----");
   });
-
-  // events.map(event => {
-  //   let { cToken, account } = event.args;
-  //   accounts.push(account);
-  // });
-
-  // let _accounts: string[] = _.uniq(accounts);
-  // console.log("Unique Accounts : ", _accounts.length);
-  // return _accounts;
 }
 
 async function getTokenSymbol(_token: string): Promise<string> {
@@ -96,7 +112,7 @@ async function getTokenSymbol(_token: string): Promise<string> {
   return symbol;
 }
 
-DB_QUEE.process(200, async (job: any, done: any) => {
+DB_QUEE.process(QUEE_CONCURRENCY, async (job: any, done: any) => {
   //   console.log(job.data);
   //   console.log("------------------------");
   // let { pair } = job.data;
@@ -108,9 +124,9 @@ DB_QUEE.process(200, async (job: any, done: any) => {
   console.log(token0, token1, pair);
   console.log("---------------------");
 
-  // let token0Symbol = await getTokenSymbol(token0);
-  // let token1Symbol = await getTokenSymbol(token1);
-  // await storePair(EXCHNAGE_NAME, token0, token1, token0Symbol, token1Symbol, pair);
+  let token0Symbol = await getTokenSymbol(token0);
+  let token1Symbol = await getTokenSymbol(token1);
+  await storePair(EXCHNAGE_NAME, token0, token1, token0Symbol, token1Symbol, pair);
 
   done(null, job);
 });
@@ -121,3 +137,25 @@ DB_QUEE.on("completed", async (job, result) => {
   }
   job.remove();
 });
+
+async function queeMonitor(QUEE: Queue.Queue<any>) {
+  let counter = 0;
+  setInterval(function () {
+    DB_QUEE.getJobCounts()
+      .then(function (result) {
+        console.log("\r" + "Queue status: ", result);
+        console.log("\r" + "Counter: ", counter, "Running Time : ", (counter * 10) / 60, "Minutes");
+        counter++;
+
+        console.log("----------");
+        if (result.active === 0 && counter > 10) {
+          console.log("Process Finished !!! FLUSH REDIS DATABASE !!!");
+          DB_QUEE.empty();
+          return true;
+        }
+      })
+      .catch(function () {
+        console.log("Error in finding out the status of the queue");
+      });
+  }, 5000);
+}
